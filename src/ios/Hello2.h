794c02d45f66e478c5e8719854e22b96f0895686
enoch1 <enoch1@zongrude-MacBook-Pro.local>
2018-03-30 13:58:26 +0800

    first comment

---
+++ src/ios/Hello2.h
@@ -0,0 +1,11 @@
+
+/********* Hello2.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface Hello2 : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
